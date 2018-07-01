328d1ff3c5c231c29a9d997f5869ee2272a561db
Tom Hunt <tomdicksonhunt@gmail.com>
2018-07-01 17:42:30 -0600

    Setup for packaging

--- setup.py
+++ setup.py
@@ -2,14 +2,26 @@
 
 from setuptools import setup
 
+with open('README.rst') as rm:
+    long_description = rm.read()
+
 setup(
     name='qtoml',
     version='0.1.0',
+    author="alethiophile",
+    author_email="tomdicksonhunt@gmail.com",
     description="New TOML encoder/decoder",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     license='MIT',
     packages=['qtoml'],
-    install_requires=[ ],
+    url="https://github.com/alethiophile/qtoml",
+    install_requires=['click'],
     python_requires='~=3.6',
-    entry_points={
-    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    )
 )
