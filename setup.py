cb997e7d364b008fd12978ee4b30da1dd7a2c9be
Tom Hunt <tomdicksonhunt@gmail.com>
2018-07-01 19:12:08 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as rm:
 
 setup(
     name='qtoml',
-    version='0.1.0',
+    version='0.1.1',
     author="alethiophile",
     author_email="tomdicksonhunt@gmail.com",
     description="New TOML encoder/decoder",
