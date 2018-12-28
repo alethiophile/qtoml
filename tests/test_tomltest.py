27e33e75f3fe0c2bd08831c227467ec5543912e4
Tom Hunt <tomdicksonhunt@gmail.com>
2018-12-27 19:35:39 -0700

    Add test to ensure against stray print statements

--- tests/test_tomltest.py
+++ tests/test_tomltest.py
@@ -2,6 +2,8 @@
 
 import pytest, json, math
 import qtoml
+from io import StringIO
+from contextlib import redirect_stdout, redirect_stderr, contextmanager
 from qtoml.__main__ import untag
 
 def patch_floats(d):
@@ -14,24 +16,42 @@ def patch_floats(d):
     else:
         return d
 
+@contextmanager
+def assert_no_output():
+    """This makes sure debug print statements I might add don't accidentally get
+    into released versions.
+
+    """
+    so, se = StringIO(), StringIO()
+    try:
+        with redirect_stdout(so), redirect_stderr(se):
+            yield
+    finally:
+        assert so.getvalue() == ''
+        assert se.getvalue() == ''
+
 def test_valid_decode(valid_case):
-    json_val = untag(json.loads(valid_case['json']))
-    toml_val = qtoml.loads(valid_case['toml'])
+    with assert_no_output():
+        json_val = untag(json.loads(valid_case['json']))
+        toml_val = qtoml.loads(valid_case['toml'])
     # some test cases include floats with value NaN, which compare unequal to
     # themselves and thus break a plain comparison
     assert patch_floats(toml_val) == patch_floats(json_val)
 
 def test_invalid_decode(invalid_decode_case):
-    with pytest.raises(qtoml.decoder.TOMLDecodeError):
-        qtoml.loads(invalid_decode_case['toml'])
+    with assert_no_output():
+        with pytest.raises(qtoml.decoder.TOMLDecodeError):
+            qtoml.loads(invalid_decode_case['toml'])
 
 def test_valid_encode(valid_case):
-    json_val = untag(json.loads(valid_case['json']))
-    toml_str = qtoml.dumps(json_val)
-    toml_reload = qtoml.loads(toml_str)
+    with assert_no_output():
+        json_val = untag(json.loads(valid_case['json']))
+        toml_str = qtoml.dumps(json_val)
+        toml_reload = qtoml.loads(toml_str)
     assert patch_floats(toml_reload) == patch_floats(json_val)
 
 def test_invalid_encode(invalid_encode_case):
-    json_val = untag(json.loads(invalid_encode_case['json']))
-    with pytest.raises(qtoml.encoder.TOMLEncodeError):
-        qtoml.dumps(json_val)
+    with assert_no_output():
+        json_val = untag(json.loads(invalid_encode_case['json']))
+        with pytest.raises(qtoml.encoder.TOMLEncodeError):
+            qtoml.dumps(json_val)
