29a41a8b5a5b083c5519ed1ba151ad34ac5d1d00
Tom Hunt <tomdicksonhunt@gmail.com>
2020-01-11 14:34:01 -0700

    Updated test config

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,24 +3,26 @@ pj = os.path.join
 
 TEST_DIR = 'toml-test/tests'
 
-def get_tomltest_cases():
+def get_tomltest_cases(rootdir):
     expect_dirs = ['invalid', 'invalid-encoder', 'valid']
-    dirs = sorted(os.listdir(TEST_DIR))
+    realdir = pj(rootdir, TEST_DIR)
+    dirs = sorted(os.listdir(realdir))
     assert all(i in dirs for i in expect_dirs)
     rv = {}
     for d in expect_dirs:
         rv[d] = {}
-        files = os.listdir(pj(TEST_DIR, d))
+        files = os.listdir(pj(realdir, d))
         for f in files:
             bn, ext = f.rsplit('.', 1)
             if bn not in rv[d]:
                 rv[d][bn] = {}
-            with open(pj(TEST_DIR, d, f)) as inp:
+            with open(pj(realdir, d, f)) as inp:
                 rv[d][bn][ext] = inp.read()
     return rv
 
 def pytest_generate_tests(metafunc):
-    test_list = get_tomltest_cases()
+    rootdir = metafunc.config.rootdir
+    test_list = get_tomltest_cases(rootdir)
     if 'valid_case' in metafunc.fixturenames:
         metafunc.parametrize('valid_case', test_list['valid'].values(),
                              ids=list(test_list['valid'].keys()))
